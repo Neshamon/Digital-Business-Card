846e2a9c53dd727e9e6ba2cadcd4ec4ca7ded2b7
John Matthews <neshamon@protonmail.com>
2022-06-27 20:24:26 -0500

    Create Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Footer() {
+  return (
+  	<footer>
+    	<btn className = "info-btn-linkedin">Linked In</btn>
+    	<btn className = "info-btn-email">Email</btn>
+    </footer>
+  )
+}
