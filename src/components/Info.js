79610a60b9d34a956954a197bef652304ca28063
John Matthews <neshamon@protonmail.com>
2022-06-27 20:24:35 -0500

    Create Info component

---
+++ src/components/Info.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Info() {
+  return (
+  	<header>
+    	<img src = ""/>
+    	<h3 className = "info-title">Name</p>
+    	<h4 className = "info-job">Job Title</p>
+    	<h5 classname = "info-portfolio" href="cool.com">portfoliowebsite.com</a>
+    	<btn className = "info-btn-linkedin">Linked In</btn>
+    	<btn className = "info-btn-email">Email</btn>
+    </header>
+  )
+}
