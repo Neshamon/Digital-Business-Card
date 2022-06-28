ff8b0809ebaa575f4a37eb21b6a84184c630558a
John Matthews <neshamon@protonmail.com>
2022-06-27 20:35:42 -0500

    Fix h3 & h4 tag

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,12 +1,12 @@
-import React from "react"
+import React from "react";
 
 export default function Info() {
   return (
   	<header>
-    	<img src = ""/>
-    	<h3 className = "info-title">Name</p>
-    	<h4 className = "info-job">Job Title</p>
-    	<h5 classname = "info-portfolio" href="cool.com">portfoliowebsite.com</a>
+    	<img className = "myself" src = ""/>
+    	<h3 className = "info-title">Name</h3>
+    	<h4 className = "info-job">Job Title</h4>
+    	<h5 classname = "info-portfolio"><a href="#">portfoliowebsite.com</a></h5>
     	<btn className = "info-btn-linkedin">Linked In</btn>
     	<btn className = "info-btn-email">Email</btn>
     </header>
