34eed868a26ee2d43ab6147efa974cdb4f60c69f
John Matthews <neshamon@protonmail.com>
2022-06-27 20:24:17 -0500

    Create About component

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function About() {
+  return (
+  	<section className = "about">
+      <h3 className = "about-title">About</h3>
+    	<p className = "about-me">About</p>
+    </section>
+  )
+}
