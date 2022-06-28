fbe94ead43f59526542f22ab182c6ec653827aef
John Matthews <neshamon@protonmail.com>
2022-06-27 20:24:49 -0500

    Create Interests component

---
+++ src/components/Interests.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Interests() {
+  return (
+  	<section className = "interests">
+    	<h3 className = "interests-title">Interests</h3>
+    	<p className = "info-job">Interests go here</p>
+    </section>
+  )
+}
