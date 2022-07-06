dd35988787ebc0ef1c22af35d0b12537a0e1fa2a
John Matthews <neshamon@protonmail.com>
2022-07-06 00:50:14 -0500

    Assign me to a variable

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,9 +1,10 @@
+import me from "../images/me.jpg"
 import React from "react";
 
 export default function Info() {
   return (
   	<header className = "info-container">
-    	<img className = "myself" src = "" alt="John Matthews"/>
+    	<img className = "myself" src = {me} alt="John Matthews" />
     	<h3 className = "info-title">Name</h3>
     	<h4 className = "info-job">Job Title</h4>
     	<h5 classname = "info-portfolio"><a href="#">portfoliowebsite.com</a></h5>
