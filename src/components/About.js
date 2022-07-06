d07fda8f282500b8027198c8b2e23d922b5c9c46
John Matthews <neshamon@protonmail.com>
2022-07-06 02:18:06 -0500

    Change border color

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
   return (
   	<section className = "about-container">
       <h3 className = "about-title">About</h3>
-    	<p className = "about-me">About</p>
+    	<p className = "about-me">Aenean ullamcorper orci et vulputate fermentum.</p>
     </section>
   )
 }
--- src/components/Interests.js
+++ src/components/Interests.js
@@ -4,7 +4,7 @@ export default function Interests() {
   return (
   	<section className = "interests-container">
     	<h3 className = "interests-title">Interests</h3>
-    	<p className = "my-interests">Interests go here</p>
+    	<p className = "my-interests">Quisque vitae varius ex, eu volutpat orci.</p>
     </section>
   )
 }
