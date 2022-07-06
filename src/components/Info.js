1ca37a026405584dc06369d1b4b732ccbcc2d26b
John Matthews <neshamon@protonmail.com>
2022-07-06 02:09:10 -0500

    Fill in info section

--- src/components/Info.js
+++ src/components/Info.js
@@ -5,8 +5,8 @@ export default function Info() {
   return (
   	<header className = "info-container">
     	<img className = "myself" src = {me} alt="John Matthews" />
-    	<h3 className = "info-title">Name</h3>
-    	<h4 className = "info-job">Job Title</h4>
+    	<h3 className = "info-title">John Matthews</h3>
+    	<h4 className = "info-job">Full Stack Web Developer</h4>
     	<h5 classname = "info-portfolio"><a href="#">portfoliowebsite.com</a></h5>
     	<btn className = "info-btn-linkedin">Linked In</btn>
     	<btn className = "info-btn-email">Email</btn>
