398522a38128d452a5386be29077428c072890c1
John Matthews <neshamon@protonmail.com>
2022-07-12 14:15:29 -0500

    Update Interests text

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -4,7 +4,12 @@ export default function Interests() {
   return (
   	<section className = "interests-container">
     	<h3 className = "interests-title">Interests</h3>
-    	<p className = "my-interests">Quisque vitae varius ex, eu volutpat orci.</p>
+    	<p className = "my-interests">
+        I am very interested in learning programming and human languages like 
+        JavaScript, Lua, Ruby, and Python. Other things are love are 
+        Blockchain development, Linux, Martial Arts, Music Theory, Singing, and
+        Fitness!
+      </p>
     </section>
   )
 }
