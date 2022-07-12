12f59d5bcf83182eb7efcfea0fd8f7aee7c90713
John Matthews <neshamon@protonmail.com>
2022-07-12 14:03:46 -0500

    Update About text

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,8 @@ export default function About() {
   return (
   	<section className = "about-container">
       <h3 className = "about-title">About</h3>
-    	<p className = "about-me">Aenean ullamcorper orci et vulputate fermentum.</p>
+    	<p className = "about-me">I am a passionate Full Stack Web Developer knowledgeable in user interface, testing, and debugging processes.
+    	 Bringing forth expertise in design, installation, testing and maintenance of web systems. </p>
     </section>
   )
 }
